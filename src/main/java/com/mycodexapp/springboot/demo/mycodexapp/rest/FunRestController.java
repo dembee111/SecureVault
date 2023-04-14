f5025fb5b70f16b52d988c135c5372c76ecaa0a2
Dembee <dembee@Dembees-MacBook-Pro.local>
2023-04-14 13:01:19 +0800

    injecting Custom Application Properties

--- src/main/java/com/mycodexapp/springboot/demo/mycodexapp/rest/FunRestController.java
+++ src/main/java/com/mycodexapp/springboot/demo/mycodexapp/rest/FunRestController.java
@@ -1,10 +1,23 @@
 package com.mycodexapp.springboot.demo.mycodexapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    //injection properties for
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach Name is " + coachName + ", Team Name is "+ teamName;
+    }
+
     @GetMapping("/")
     public String sayHello(){
         return "Hello world";

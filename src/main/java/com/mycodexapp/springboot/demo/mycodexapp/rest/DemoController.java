073bc5aa64d6a5c13bd89ab07256937a9353628b
Dembee <dembee@Dembees-MacBook-Pro.local>
2023-04-15 12:36:33 +0800

    ===== bean lifecycle =====

--- src/main/java/com/mycodexapp/springboot/demo/mycodexapp/rest/DemoController.java
+++ src/main/java/com/mycodexapp/springboot/demo/mycodexapp/rest/DemoController.java
@@ -9,16 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     private Coach myCoach;
+    private Coach anotherCoach;
     //constructor injection
 //    @Autowired
 //    public DemoController(@Qualifier("baseballCoach") Coach theCoach){
 //        myCoach = theCoach;
 //    }
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach){
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach,
+                          @Qualifier("cricketCoach") Coach theAnotherCoach){
 
         System.out.println("In Constructor: "+ getClass().getSimpleName());
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
     /* setter injection shuu */
 //    @Autowired
@@ -29,6 +32,11 @@ public class DemoController {
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
         return myCoach.getDailyWorkout1();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing beans: myCoach === anotherCoach " + (myCoach == anotherCoach);
     }
 
 }
